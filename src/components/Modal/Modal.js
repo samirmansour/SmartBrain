9a135c2eead3b5c727967b925394dda5a60b8107
Sammy Mansour <42918370+samirmansour@users.noreply.github.com>
2019-02-27 07:59:24 -0500

    Add files via upload

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
+
+const modalRoot = document.getElementById('modal-root');
+
+
+export default class Modal extends React.Component {
+	constructor(props){
+		super(props);
+		this.el = document.createElement('div');
+	}
+	//Load up all the html into the document if this is called.
+	componentDidMount() {
+		modalRoot.appendChild(this.el);
+	}
+
+	componentWillUnmount() {
+		modalRoot.removeChild(this.el);
+	}
+
+
+	render() {
+		return ReactDOM.createPortal(this.props.children, this.el);
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
